665c6c62f3d6161c4ee5776659a283d323c11ec1
nking <nking07049925@gmail.com>
2019-11-03 18:35:20 +0300

    Fixed the motion

--- hollowKnightMap.js
+++ hollowKnightMap.js
@@ -8,19 +8,19 @@ function setup() {
 
 let circleSize = 200;
 let posX, posY;
-let speedX = 2, speedY = 3;
+let speedX = -2, speedY = -3;
 
 function draw() {
   background(0);
   circle(posX,posY,circleSize);
   posX += speedX;
   posY += speedY;
-  if (abs(posX - width/2) < width/2 - circleSize) {
-    posX = width/2 + sign(posX - width/2) * (width/2 - circleSize);
+  if (abs(posX - width/2) > width/2 - circleSize/2) {
+    posX = width/2 + sign(posX - width/2) * (width/2 - circleSize/2);
     speedX = -speedX;
   }
-  if (abs(posY - height/2) < height/2 - circleSize) {
-    posY = height/2 + sign(posY - height/2) * (height/2 - circleSize);
+  if (abs(posY - height/2) > height/2 - circleSize/2) {
+    posY = height/2 + sign(posY - height/2) * (height/2 - circleSize/2);
     speedY = -speedY;
   }
 }
