a4ebc5e2f32b997d8ae311e2ce79325e359db2d8
nking <nking07049925@gmail.com>
2019-11-07 19:19:16 +0300

    Smoothing add

--- hollowKnightMap.js
+++ hollowKnightMap.js
@@ -1,6 +1,7 @@
-let version = '0.05'
+let version = '0.06'
 
 let scale = 1;
+let desiredScale = 1;
 let minScale = 1;
 let maxScale = 5;
 let defSize = 400;
@@ -30,6 +31,8 @@ function setup() {
   constrainMargin = defSize * 0.2;
   posX = windowWidth/2-defSize/2;
   posY = windowHeight/2-defSize/2;
+  vx = 0;
+  vy = 0;
 
   div.position(posX, posY);
   div.size(defSize, defSize);
@@ -46,12 +49,19 @@ function setup() {
 }
 
 let posX, posY;
+let vx, vy;
 
 function draw() {
   clear();
+  updateScale();
+  posX += vx;
+  posY += vy;
   div.position(posX,posY);
   circle(posX+0*scale, posY+0*scale, 10 * scale+30);
   text('Version '+version,0,40);
+  let velK = map(scale, minScale, maxScale, 0.7, 0.9);
+  vx *= velK;
+  vy *= velK;
 }
 
 let constrainMargin;
@@ -63,6 +73,10 @@ function constrainPos() {
 
 function mousePressed() {
   cursor('GRABBING');
+  maxVel = 0;
+  vx = 0;
+  vy = 0;
+  vels = [];
 }
 
 let pscale = 1;
@@ -70,10 +84,40 @@ let pscale = 1;
 function mouseDragged() {
   posX += mouseX - pmouseX;
   posY += mouseY - pmouseY;
+  let tx = mouseX - pmouseX;
+  let ty = mouseY - pmouseY;
+  let len = mag(tx, ty);
+  vels.push(len);
+  if (vels.length > 5) vels.shift();
   constrainPos();
 }
 
+let vels = [];
+
+function getMax(arr) {
+  if (arr[0]) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > max) max = arr[i];
+    }
+    return max;
+  }
+  return 0;
+}
+
 function mouseReleased() {
+  let dx = mouseX - pmouseX;
+  let dy = mouseY - pmouseY;
+  if (dx === 0 && dy === 0) {
+    vx = 0;
+    vy = 0;
+  } else {
+    let len = mag(dx, dy);
+    let maxVel = getMax(vels);
+    vx = dx * maxVel / len;
+    vy = dy * maxVel / len;
+  }
+  
   cursor('GRAB');
 }
 
@@ -81,9 +125,14 @@ let scrollSpeed = 0;
 
 function mouseWheel(event) {
   if (scrollSpeed === 0) scrollSpeed = abs(event.deltaY);
+  desiredScale *= pow(0.85, event.deltaY/scrollSpeed);
+  desiredScale = constrain(desiredScale, minScale, maxScale);
+  return false;
+}
+
+function updateScale() {
   pscale = scale;
-  scale *= pow(0.85, event.deltaY/scrollSpeed);
-  scale = constrain(scale, minScale, maxScale);
+  scale = lerp(scale, desiredScale, 0.3);
   posX = (posX - mouseX) * scale / pscale + mouseX;
   posY = (posY - mouseY) * scale / pscale + mouseY;
   curSize = defSize * scale;
@@ -93,7 +142,6 @@ function mouseWheel(event) {
   div.size(curSize, curSize);
   div.child()[0].width = curSize;
   div.child()[0].height = curSize;
-  return false;
 }
 
 function windowResized() {
